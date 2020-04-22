d9d39567f12ed61d9107da5f9e346c93dbc2379d
Linux Developer for android <linux_dentalpockets@comsec.co.in>
2020-04-22 21:55:29 +0530

    Finished the Challenge

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +17,45 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int _leftNumber = 1;
+  int _rightNumber = 6;
+
+  Random rnd = new Random();
+  int min = 1;
+  int max = 6;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Center(
+        child: Row(
+          children: [
+            expandedImage(_leftNumber, 'L'),
+            expandedImage(_rightNumber, 'R'),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Expanded expandedImage(int image, String which) {
+    return Expanded(
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            _leftNumber = min + rnd.nextInt(max - min);
+            _rightNumber = Random().nextInt(6) + 1;
+          });
+//          d.log('data:$_leftNumber & $_rightNumber - $which clicked');
+        },
+        child: Image.asset('images/dice' + image.toString() + '.png'),
+      ),
+    );
   }
 }
